7a74e8f1133d7ef79c145fc6f3d26cd305b5e972
Muhammad Sallam <qeymax@gmail.com>
2016-10-26 17:48:53 +0200

    Added books to database

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ var ajaxBooks = []
 $(document).ready(function () {
   $('.ui.accordion').accordion()
   $('.ui.checkbox').checkbox()
-  $('.ui.large.modal').modal('show')
+
   $('.closeModal').click(function () {
     $('.modal').modal('hide')
   })
@@ -17,21 +17,50 @@ $(document).ready(function () {
   $('#addNewBookForm').submit(function (event) {
     event.preventDefault()
     $('#addNewBookSegment').addClass('loading')
-    $
-      .ajax({
-        url: '/books?bookname=' + $('#bookNameInput').val() + '',
-        type: 'POST',
-        error: function (xhr, status, error) {
-          console.log(error)
-        },
-        success: function (result, status, xhr) {
-          console.log(result)
+    if ($('#bookNameInput').val() !== '') {
+      getBooks($('#bookNameInput').val())
+    }
+  })
+
+  $('.bookCard').on('click',
+    function () {
+      $('.ui.large.modal').modal('show')
+      $('.segment').addClass('loading')
+      $('.bookNameSpan').text($(this).children('h5').text())
+      getBookInfo($(this).attr('id'))
+    })
+})
+
+var getBooks = function (bookName) {
+  $
+    .ajax({
+      url: '/addbooks?bookname=' + bookName + '',
+      type: 'GET',
+      error: function (xhr, status, error) {
+        console.log(error)
+        $('.modal').modal('hide')
+        $('#addNewBookSegment').removeClass('loading')
+      },
+      success: function (result, status, xhr) {
+        if (typeof result !== 'object') {
+          $('#addNewBookSegment').removeClass('loading')
+          let html = `<h1>You need to logged in to do that.</h1>`
+          $('#addNewBookGrid').html(html)
+        } else {
           ajaxBooks = result.volumes
-          let html = ``
-          for (let volume of result.volumes) {
-            html += `<div class="sixteen wide mobile eight wide tablet four wide computer column">
+          showBooks(result.volumes)
+          $('#addNewBookSegment').removeClass('loading')
+        }
+      }
+    })
+}
+
+var showBooks = function (volumes) {
+  let html = ``
+  for (let volume of volumes) {
+    html += `<div class="sixteen wide mobile eight wide tablet four wide computer column">
                     <div class="card choiceCard">
-                    <input style="display:none" id="bookNumber" type="text" value="${result.volumes.indexOf(volume)}">
+                    <input style="display:none" id="bookNumber" type="text" value="${volumes.indexOf(volume)}">
                       <div class="image">
                         <img class="thumbnail" src="${volume.thumbnail}"
                         alt="">
@@ -41,15 +70,14 @@ $(document).ready(function () {
                       </a>
                     </div>
                   </div>`
-          }
-          $('#addNewBookGrid').html(html)
-          $('.choiceCard').on('click', function () {
-            $('.modal').modal('hide')
-            let i = $(this).children('#bookNumber').val()
-            console.log(i)
-            let html = ``
-            html += `<div class="sixteen wide mobile eight wide tablet four wide computer column">
-                    <div class="card">
+  }
+  $('#addNewBookGrid').html(html)
+  $('.choiceCard').on('click', function () {
+    $('.modal').modal('hide')
+    let i = $(this).children('#bookNumber').val()
+    let html = ``
+    html += `<div class="sixteen wide mobile eight wide tablet four wide computer column">
+                    <div id="<%= volume._id %>" class="card bookCard">
                       <div class="image">
                         <img class="thumbnail" src="${ajaxBooks[i].thumbnail}"
                         alt="">
@@ -59,10 +87,40 @@ $(document).ready(function () {
                       </a>
                     </div>
                   </div>`
-            $('.booksGrid').prepend(html)
-          })
-          $('#addNewBookSegment').removeClass('loading')
-        }
-      })
+    $('.booksGrid').prepend(html)
+    sendBook(i)
   })
-})
+}
+
+var sendBook = function (index) {
+  $
+    .ajax({
+      url: '/books',
+      type: 'POST',
+      data: ajaxBooks[index],
+      error: function (xhr, status, error) {
+        console.log(error)
+      },
+      success: function (result, status, xhr) {
+
+      }
+    })
+}
+
+var getBookInfo = function (id) {
+  $
+    .ajax({
+      url: '/bookinfo',
+      type: 'GET',
+      data: {
+        id: id
+      },
+      error: function (xhr, status, error) {
+        console.log(error)
+      },
+      success: function (result, status, xhr) {
+        $('.segment').html(result)
+        $('.segment').removeClass('loading')
+      }
+    })
+}
--- routes/books.js
+++ routes/books.js
@@ -1,48 +1,111 @@
 var router = require('express').Router()
 var request = require('request')
+var middleware = require('../middleware')
+var Book = require('../models/book')
   // var path = require('path')
   // var passport = require('passport')
 
 router.route('/books')
   .get(function (req, res) {
-    res.render('books')
-  })
-  .post(function (req, res) {
-    let bookName = req.query.bookname
-    let url = `https://www.googleapis.com/books/v1/volumes?q=${bookName}&maxResults=3&key=AIzaSyBhCDWa_59Ncnq-HbtSNstn68RKkOSPXQY`
-    request(url, function (err, response, body) {
-      if (err) {
-        console.log(err)
-      }
-      let obj = JSON.parse(body)
-      var volumes = []
-      for (let item of obj.items) {
-        let volumeInfo = item.volumeInfo
-        var thumbnail = '/img/default-cover.jpg'
-        if (volumeInfo.imageLinks) {
-          thumbnail = volumeInfo.imageLinks.thumbnail.split('&')
-          thumbnail[3] = 'zoom=2'
-          thumbnail = thumbnail.join('&')
-        }
-        let subtitle = volumeInfo.subtitle ? ' - ' + volumeInfo.subtitle.substring(0, 20) : ''
-        let description = volumeInfo.description ? volumeInfo.description.substring(0, 100) + '....' : ''
-        let volume = {
-          title: volumeInfo.title.split('-')[0] + subtitle,
-          author: volumeInfo.authors,
-          genre: volumeInfo.categories,
-          thumbnail: thumbnail,
-          language: volumeInfo.language,
-          publishedDate: volumeInfo.publishedDate,
-          averageRating: volumeInfo.averageRating,
-          description: description
-        }
-        volumes.push(volume)
-      }
-
-      res.json({
-        volumes: volumes
+    Book.find({})
+      .limit(10).sort({
+        title: -1
       })
+      .exec(function (err, books) {
+        if (err) {
+          console.log(err)
+        } else {
+          res.render('books', {
+            volumes: books
+          })
+        }
+      })
+  })
+  .post(middleware.isLoggedIn,
+    function (req, res) {
+      let newBook = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        genre: req.body.genre,
+        thumbnail: req.body.thumbnail,
+        language: req.body.language,
+        publishedDate: req.body.publishedDate,
+        averageRating: req.body.averageRating,
+        description: req.body.description,
+        user: req.user
+      })
+      newBook.save(function (err, book) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('new book saved to database : ', book.title)
+        }
+      })
+      console.log(req.body.title)
+      res.end()
     })
+
+router.route('/addbooks')
+  .get(middleware.isLoggedIn,
+    function (req, res) {
+      let bookName = req.query.bookname
+      if (bookName) {
+        let url = `https://www.googleapis.com/books/v1/volumes?q=${bookName}&maxResults=3&key=AIzaSyBhCDWa_59Ncnq-HbtSNstn68RKkOSPXQY`
+        request(url, function (err, response, body) {
+          if (err) {
+            console.log(err)
+          }
+          let obj = JSON.parse(body)
+          var volumes = []
+          for (let item of obj.items) {
+            let volumeInfo = item.volumeInfo
+            var thumbnail = '/img/default-cover.jpg'
+            if (volumeInfo.imageLinks) {
+              thumbnail = volumeInfo.imageLinks.thumbnail.split('&')
+              thumbnail[3] = 'zoom=2'
+              thumbnail = thumbnail.join('&')
+            }
+            let subtitle = volumeInfo.subtitle ? ' - ' + volumeInfo.subtitle : ''
+            let description = volumeInfo.description ? volumeInfo.description.substring(0, 100) + '....' : ''
+            let volume = {
+              title: volumeInfo.title.split('-')[0] + subtitle,
+              author: volumeInfo.authors,
+              genre: volumeInfo.categories,
+              thumbnail: thumbnail,
+              language: volumeInfo.language,
+              publishedDate: volumeInfo.publishedDate,
+              averageRating: volumeInfo.averageRating,
+              description: description
+            }
+            volumes.push(volume)
+          }
+
+          res.json({
+            volumes: volumes
+          })
+        })
+      } else {
+        res.end()
+      }
+    })
+
+router.route('/bookinfo')
+  .get(function (req, res) {
+    if (req.query.id !== null) {
+      Book.findById(req.query.id).populate('user').exec(function (err, book) {
+        if (err) {
+          console.log(err)
+        } else {
+          if (book) {
+            res.render('partials/modal', {
+              volume: book
+            })
+          } else {
+            res.send('<h1> Something went wrong, please try again or contact someone... not me anyone else.</h1>')
+          }
+        }
+      })
+    }
   })
 
 module.exports = router
